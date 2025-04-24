75358322b9a16b91633ed7d75984bc66806ca4ff
escapeuw <ghkdehd78999@gmail.com>
2025-04-24 01:11:24 -0400

    initial commit

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/escapeuw/eatwhat/backend/db"
+	"github.com/escapeuw/eatwhat/backend/models"
+	"github.com/escapeuw/eatwhat/backend/handlers"
+  )
+
+  func main() {
+	db.ConnectDB()
+	db.DB.AutoMigrate(&models.User{}, &models.Mood{}, &models.Feedback{})
+  
+	r := gin.Default()
+	r.POST("/api/suggest", handlers.HandleSuggest)
+  
+	r.Run() // Runs on :8080 by default
+  }
